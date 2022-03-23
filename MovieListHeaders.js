e52ac8d650c7257eccb778e4417cec9209325255
kjetsv17 <102188524+kjetsv17@users.noreply.github.com>
2022-03-23 01:31:18 +0100

    Create MovieListHeaders.js

---
+++ MovieListHeaders.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const MovieListHeaders = (props) => {
+    return(
+        <div className='col'>
+            <h1>{props.heading}</h1>
+        </div>
+    );
+};
+
+export default MovieListHeaders;
