2183186399627c52743f4d37dc510071831fe5ad
kjetsv17 <102188524+kjetsv17@users.noreply.github.com>
2022-03-23 17:12:03 +0100

    Update MovieList.js

--- MovieList.js
+++ MovieList.js
@@ -1,16 +1,15 @@
-import react from 'react';
+import React from 'react';
 
 const MovieList = (props) => {
-    return(
-        <>
-            {props.movies.map((movie, index)=> (
-            <div className = 'image container d-flex justify-content-start m-3'>
-                <img src=(movie.poster) alt= 'movie'></img>
-                #<div className= "overlay d-flex align-items-center justify-content"> </div>
-            </div>
-            ))}
-        </>
-    );
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+				</div>
+			))}
+		</>
+	);
 };
 
 export default MovieList;
